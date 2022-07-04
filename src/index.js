5f1101cfa244701f16f292fc917f96bf6614c7a3
codeindevelopment <codeindevelopment@gmail.com>
2022-07-05 03:41:21 +0430

    implement json middleware for express

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import express from "express";
+import express, { json } from "express";
 import chalk from "chalk";
 
 const app = express();
 const PORT = process.env.port || 8080;
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("ok");
+  res.status(200).json({ message: "system ok" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
   console.log(
     `👋 Server is running at ${chalk.green(`https://localhost:${PORT}`)} `
   );
